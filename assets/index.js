d16df2303b36fde52b276685e57972d436495083
johnny <jvasconcelosbittencourt@gmail.com>
2023-01-16 19:22:01 -0300

    catalogando produtos

--- assets/index.js
+++ assets/index.js
@@ -2,12 +2,22 @@ const vm = new Vue({
     el: "#wrapper",
     data: {
         counter: 0,
-        preco: 120 - 10
+        preco: 120 - 10,
+        produtos: {}
     },
     methods: {
-
+        produtosFetch() {
+            fetch("./api/produtos.json")
+            .then(r => r.json())
+            .then(r => {
+                this.produtos = r
+            })
+        }
     },
     computed: {
         
+    },
+    created() {
+        this.produtosFetch()
     }
 })
