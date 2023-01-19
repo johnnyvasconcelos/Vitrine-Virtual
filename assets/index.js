93e51f15db7b9772a590771989b9710cf460a1e4
johnny <jvasconcelosbittencourt@gmail.com>
2023-01-18 21:34:44 -0300

    adicionando produtos + local storage

--- assets/index.js
+++ assets/index.js
@@ -6,7 +6,8 @@ const vm = new Vue({
         produtos: {},
         showMenu: false,
         scrollNumber: 0,
-        produtoModal: false
+        produtoModal: false,
+        carrinho: []
     },
     filters: {
         numeroPreco(valor) {
@@ -36,6 +37,19 @@ const vm = new Vue({
             console.log(promo[1])
             alert('promo ok')
         },
+        adicionarItem() {
+            this.produtoModal.estoque --
+            const {id, nome, preco} = this.produtoModal
+            this.carrinho.push({id, nome, preco})
+        },
+        removerItem(index) {
+            this.carrinho.splice(index, 1)
+        },
+        checarLocalstorage() {
+            if (window.localStorage.carrinho) {
+                this.carrinho = JSON.parse(window.localStorage.carrinho)
+            }
+        },
         handleScroll() {
             let nav = document.querySelector('#nav')
             this.scrollNumber = window.scrollY
@@ -46,9 +60,26 @@ const vm = new Vue({
             }
         }
     },
+    computed: {
+        carrinhoTotal() {
+            total = 0
+            if (this.carrinho.length) {
+                this.carrinho.forEach(item => {
+                    total += item.preco
+                })
+            }
+            return total
+        }
+    },
+    watch: {
+        carrinho() {
+            window.localStorage.carrinho = JSON.stringify(this.carrinho)
+        }
+    },
     created() {
         this.produtosFetch(),
         window.addEventListener('scroll', this.handleScroll)
+        this.checarLocalstorage()
     },
     mounted() {
         this.$nextTick(() => {
